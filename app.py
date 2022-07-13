8242b9cc3322c283bc1d1037462f6915ca87e297
noopy <young_ahn@yonsei.ac.kr>
2022-07-13 13:56:40 +0900

    update with stabilized performance

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ en2ko_model = AutoModelForSeq2SeqLM.from_pretrained("QuoQA-NLP/KE-T5-En2Ko-Base"
 st.title("🤖 KoQuillBot")
 
 
-default_value = "한국어 문장 변환기 QuillBot입니다."
+default_value = "이건 한국어 문장 변환기 QuillBot입니다."
 src_text = st.text_area(
     "바꾸고 싶은 문장을 입력하세요:",
     default_value,
@@ -26,60 +26,40 @@ src_text = st.text_area(
 print(src_text)
 
 
-def infer_sentence(model, src_text, tokenizer=tokenizer):
-    encoded_prompt = tokenizer.encode(
-        src_text,
-        add_special_tokens=False,
-        return_tensors="pt",
-        padding=True,
-        max_length=64,
-    )
-    if encoded_prompt.size()[-1] == 0:
-        input_ids = None
-    else:
-        input_ids = encoded_prompt
-
-    output_sequences = model.generate(
-        input_ids=input_ids,
-        max_length=64,
-        num_beams=5,
-        repetition_penalty=1.3,
-        no_repeat_ngram_size=3,
-        num_return_sequences=1,
-    )
-    print(output_sequences)
-
-    generated_sequence = output_sequences[0]
-    print(generated_sequence)
-
-    # Decode text
-    text = tokenizer.decode(
-        generated_sequence, clean_up_tokenization_spaces=True, skip_special_tokens=True
-    )
-    print(text)
-
-    # Remove all text after the pad token
-    stop_token = tokenizer.eos_token
-    text = text[: text.find(stop_token) if stop_token else None]
-    text = text.strip()
-    return text
-
 
 if st.button("문장 변환") or src_text == default_value:
     if src_text == "":
         st.warning("Please **enter text** for translation")
 
     else:
-        st.success("Translating...")
-        english_translation = infer_sentence(
-            model=ko2en_model, src_text=src_text, tokenizer=tokenizer
+        # translate into english sentence
+        english_translation = ko2en_model.generate(
+            **tokenizer(
+                src_text,
+                return_tensors="pt",
+                padding="max_length",
+                truncation=True,
+                max_length=64,
+            ),
+            max_length=64,
+            num_beams=5,
+            repetition_penalty=1.3,
+            no_repeat_ngram_size=3,
+            num_return_sequences=1,
+        )
+        english_translation = tokenizer.decode(
+            english_translation[0],
+            clean_up_tokenization_spaces=True,
+            skip_special_tokens=True,
         )
 
+        # translate back to korean
         korean_translation = en2ko_model.generate(
             **tokenizer(
                 english_translation,
                 return_tensors="pt",
-                padding=True,
+                padding="max_length",
+                truncation=True,
                 max_length=64,
             ),
             max_length=64,
@@ -94,7 +74,7 @@ if st.button("문장 변환") or src_text == default_value:
             clean_up_tokenization_spaces=True,
             skip_special_tokens=True,
         )
-        st.success(f"{src_text} -> {english_translation} -> {korean_translation}")
+        print(f"{src_text} -> {english_translation} -> {korean_translation}")
 else:
     pass
 
